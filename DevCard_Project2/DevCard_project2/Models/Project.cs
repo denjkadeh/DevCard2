f4ff4a022deea996112234350ff8e8d84c26f5cd
Ahmad <Ahmad@DESKTOP-RN9UCIR>
2022-07-04 09:15:00 +0430

    کامپوننت

--- DevCard_Project2/DevCard_project2/Models/Project.cs
+++ DevCard_Project2/DevCard_project2/Models/Project.cs
@@ -12,5 +12,13 @@ namespace DevCard_project2.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public string Client { get; set; }
+
+        public Project(long id, string name, string description, string client)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Client = client;
+        }
     }
 }
---
+++ DevCard_Project2/DevCard_project2/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+
+namespace DevCard_project2.ViewComponents
+{
+    public class ProjectsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var projects = new List<Project >
+            {
+                new Project()
+            };
+            return View("_Projects");
+        }
+
+    }
+}
